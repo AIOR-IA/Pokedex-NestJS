c30328162608aa38de2f3e9ee07924ad57db5d10
Lenar Lima <lenar.toledo@gmail.com>
2024-01-12 00:24:44 -0400

    added pagination in GetPokemons

--- src/seed/seed.service.ts
+++ src/seed/seed.service.ts
@@ -1,21 +1,21 @@
 import { Injectable } from '@nestjs/common';
-import axios, { AxiosInstance } from 'axios';
 import { PokeResponse } from './interface/pokemon.interface';
 import { PokemonService } from '../pokemon/pokemon.service';
 import { Pokemon } from 'src/pokemon/entities/pokemon.entity';
+import { AxiosAdapter } from 'src/common/http-adapters/axios.adapter';
 
 @Injectable()
 export class SeedService {
 
-  constructor(private pokemonService: PokemonService) {
+  constructor(
+    private pokemonService: PokemonService,
+    private readonly http: AxiosAdapter
+  ) {}
 
-  }
-
-  private readonly axios: AxiosInstance = axios;
 
   async executeSeed() {
     const inserAllPromiseArrary = [];
-    const { data } = await this.axios.get<PokeResponse>(`https://pokeapi.co/api/v2/pokemon?limit=500`);
+    const data = await this.http.get<PokeResponse>(`https://pokeapi.co/api/v2/pokemon?limit=500`);
     const res = data.results.map( pokemon => {
       return {
         name:pokemon.name,
